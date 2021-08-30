a8d4844e1a13280226ba30cc3b01a6c85a67c67c
LilianLem <54894780+LilianLem@users.noreply.github.com>
2021-08-30 12:23:09 +0200

    Removing variable types checks

--- src/MathLib.ts
+++ src/MathLib.ts
@@ -1,43 +1,43 @@
-import {Utils} from "./Utils";
+// import {Utils} from "./Utils";
 
 export class MathLib
 {
     public static sum(number1:number, number2:number)
     {
-        if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
+        /*if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
         {
             throw new Error("At least one argument is not a number !");
-        }
+        }*/
 
         return number1+number2;
     }
 
     public static substract(number1:number, number2:number)
     {
-        if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
+        /*if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
         {
             throw new Error("At least one argument is not a number !");
-        }
+        }*/
 
         return number1-number2;
     }
 
     public static multiply(number1:number, number2: number)
     {
-        if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
+        /*if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
         {
             throw new Error("At least one argument is not a number !");
-        }
+        }*/
 
         return number1*number2;
     }
 
     public static divide(number1:number, number2: number)
     {
-        if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
+        /*if(!Utils.checkIfNumber(number1) || !Utils.checkIfNumber(number2))
         {
             throw new Error("At least one argument is not a number !");
-        }
+        }*/
 
         if(number2 === 0)
         {
